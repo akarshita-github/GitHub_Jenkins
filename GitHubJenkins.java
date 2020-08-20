c27bf4ca16aa42299423668da05bd2272f30be19
akarshita-github <69955600+akarshita-github@users.noreply.github.com>
2020-08-20 20:15:18 +1000

    Add files via upload

---
+++ GitHubJenkins.java
@@ -0,0 +1,9 @@
+
+public class GitHubJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Hello World!! My first Github and Jenkins Integration project");
+	}
+
+}
